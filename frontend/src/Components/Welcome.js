facfabb30e661dcd645b1521075ccf653584b51f
Dani <babadany2999@gmail.com>
2023-03-21 23:27:09 +0200

    added a countdown timer for important events

--- frontend/src/Components/Welcome.js
+++ frontend/src/Components/Welcome.js
@@ -1,22 +1,19 @@
 import React from 'react';
 import '../Styles/Welcome.css';
 import Buttons from './Buttons';
-import TestButtons from './TestButtons';
+import CountdownTimerWrapper from "./Wrappers_Global/CountdownTimer.js";
 
 const Welcome = () => {
   return (
     <div className="WelcomeContainer MainPage">
+      <CountdownTimerWrapper></CountdownTimerWrapper>
       
 
       <Buttons link={"diet"} name={"Diet"}></Buttons>
       <Buttons link={"training"} name={"Training"}></Buttons>
       <Buttons link={"life"} name={"Life"}></Buttons>
       <Buttons link={"calendar"} name={"Calendar"}></Buttons>
-      
-      {/* <TestButtons link={"diet"} name={"Diet"}></TestButtons>
-      <TestButtons link={"training"} name={"Training"}></TestButtons>
-      <TestButtons link={"life"} name={"Life"}></TestButtons>
-      <TestButtons link={"calendar"} name={"Calendar"}></TestButtons> */}
+
 
     </div>
   );
---
+++ frontend/src/Components/Wrappers_Global/CountdownTimer.js
@@ -0,0 +1,71 @@
+import "../../Styles/Wrappers_Global/CountdownTimer.css";
+import {useEffect, useState} from 'react';
+
+
+const CountdownTimer = ({title, time}) => {
+
+
+    const [seconds, setSeconds] = useState();
+    const [minutes, setMinutes] = useState();
+    const [hours, setHours] = useState();
+    const [days, setDays] = useState();
+
+
+    useEffect(()=>{
+        var miliseconds;
+        var interval;
+        if(time){
+            interval = setInterval(()=>{
+                const date_current = new Date();
+                // The format time is in: {day: DD, month: MM, year: YYYY}
+                const date_needed = new Date(time.year, time.month - 1, time.day);
+                const miliseconds = date_needed - date_current;
+    
+                    
+                var days = Math.floor(miliseconds / (1000 * 60 * 60 * 24));
+                var hours = Math.floor((miliseconds % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                var minutes = Math.floor((miliseconds % (1000 * 60 * 60)) / (1000 * 60));
+                var seconds = Math.floor((miliseconds % (1000 * 60)) / 1000);
+    
+                setSeconds(seconds);
+                setMinutes(minutes);
+                setHours(hours);
+                setDays(days);
+            } , 1000)
+        }
+        return () => {
+            if(miliseconds === 0){
+                clearInterval(interval)
+            }
+        }
+    } , [time])
+
+
+    return (
+        <div className="CountdownTimer">
+            <span className="CountdownTimerTitle">{title}</span>
+            <div className="CountdownTimerTime">
+                <div className="CountdownTimerTimeValue">{days}</div>
+                <div className="CountdownTimerSeparator">:</div>
+                <div className="CountdownTimerTimeValue">{hours}</div>
+                <div className="CountdownTimerSeparator">:</div>
+                <div className="CountdownTimerTimeValue">{minutes}</div>
+                <div className="CountdownTimerSeparator">:</div>
+                <div className="CountdownTimerTimeValue">{seconds}</div>
+            </div>
+        </div>
+    )
+}
+
+
+const CountdownTimerWrapper = () => {
+    return (
+        <div className="CountdownTimerWrapper">
+        <CountdownTimer title={"Start Day"} time={{day: 20, month: 5, year: 2023}}></CountdownTimer>
+        <CountdownTimer title={"D-Day"} time={{day: 20, month: 5, year: 2030}}></CountdownTimer>
+        </div>
+    )
+}
+
+
+export default CountdownTimerWrapper;
